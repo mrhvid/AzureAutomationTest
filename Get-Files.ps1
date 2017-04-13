0be31b74300a5360c97a67326db15d33f9a2517b
Jonas Sommer Nielsen <git@1d.dk>
2017-04-13 21:47:59 +0200

    First draft

---
+++ Get-Files.ps1
@@ -0,0 +1,19 @@
+ param
+(
+  # Path to square .jpg thumbnails
+  [Parameter(Mandatory=$true)]
+  [ValidateScript({Test-Path $_})]
+  [String]
+  $Path
+)
+
+$ThumbnailFiles = Get-ChildItem -Path $Path -Filter '*.jpg'
+
+foreach($File in $ThumbnailFiles) {
+
+  $NewPath = Move-Item -Path $File.FullName -Destination 'C:\temp\SetADPhoto\processing\' -Force -PassThru
+
+  #Call next runbook
+  .\ADP-SetADPhoto.ps1 -path $NewPath.FullName
+  
+}
