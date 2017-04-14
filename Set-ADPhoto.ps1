74e4530b9151785f0370f4b2a73e643a3ea67d45
Jonas Sommer Nielsen <git@1d.dk>
2017-04-14 15:05:41 +0200

    Resizing

--- Set-ADPhoto.ps1
+++ Set-ADPhoto.ps1
@@ -7,20 +7,21 @@ param
   $JpgFilePathPath
 )
 
-
     Write-Verbose -Message  "This is SetAdPhoto and the path is: $JpgFilePathPath"
 
     $File = Get-ChildItem -Path $JpgFilePathPath
     $SamAccount = $File.BaseName
     $User = Get-ADUser -Identity $SamAccount
 
-
     if($user) {
 
-        [byte[]]$Picture = Get-Content $JpgFilePathPath -Encoding byte
+        $ThumbPath = '{0}\{1}-96px{2}' -f $File.Directory, $File.BaseName, $File.Extension
+        .\Resize-File.ps1 -JpgFilePathPath $JpgFilePathPath -Height 96 -Width 96 -OutputPath $ThumbPath
+
+        [byte[]]$Picture = Get-Content $ThumbPath -Encoding byte
         $User | Set-ADUser -Replace @{thumbnailphoto=$Picture} -Credential (Get-AutomationPSCredential -Name "hyper1LabAdmin")
 
-        "File $File written to $($User.Name)"
+        'File {0} written to {1}' -f $File, $User.Name
 
     } else {
 
